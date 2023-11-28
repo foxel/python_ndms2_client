148334a5401b287f37558f08a40de8e7117bb826
Roman Zavalov <japangod@yandex.ru>
2023-11-28 15:05:36 +0300

    Added commit_failsafe_configuration method

--- ndms2_client/client.py
+++ ndms2_client/client.py
@@ -12,6 +12,7 @@ _ASSOCIATIONS_CMD = 'show associations'
 _HOTSPOT_CMD = 'show ip hotspot'
 _INTERFACE_CMD = 'show interface %s'
 _SAVE_CONFIGURATION_CMD = 'system configuration save'
+_FAILSAFE_COMMIT_CONFIGURATION_CMD = 'system configuration fail-safe commit'
 _INTERFACES_CMD = 'show interface'
 _SET_INTERFACE_STATE_CMD = 'interface {interface} {state}'
 _INTERFACE_STATE_UP = 'up'
@@ -203,6 +204,9 @@ class Client(object):
 
     def save_configuration(self):
         self._connection.run_command(_SAVE_CONFIGURATION_CMD)
+
+    def commit_failsafe_configuration(self):
+        self._connection.run_command(_FAILSAFE_COMMIT_CONFIGURATION_CMD)
 
     def set_interface_state(self, interface_id: str, is_up: bool):
         state_str = _INTERFACE_STATE_UP if is_up else _INTERFACE_STATE_DOWN
